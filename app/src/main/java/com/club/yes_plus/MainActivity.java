6b03a7a7f548a70404b9a953a6429a1d5809b766
vivek1339 <kvivek1339@gmail.com>
2018-11-10 20:55:33 +0530

    Initial commit

---
+++ app/src/main/java/com/club/yes_plus/MainActivity.java
@@ -0,0 +1,13 @@
+package com.club.yes_plus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
