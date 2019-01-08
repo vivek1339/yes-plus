a3c385ecc2d2e4417c7f13123b699e69962e83ca
harshithaputtaswamy <harshi2680@gmail.com>
2019-01-08 15:54:29 +0530

    commit for main activity and members and about_us

--- app/src/main/java/com/club/yes_plus/MainActivity.java
+++ app/src/main/java/com/club/yes_plus/MainActivity.java
@@ -1,16 +1,46 @@
 package com.club.yes_plus;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button Events;
+    Button Members;
+    Button About_Us;
+    Button Kriya;
+    Button News_Feed;
+    Button project;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView tv=new TextView();
-        tv.setText("hello");
+
+        About_Us=(Button)findViewById(R.id.About_Us);
+        Kriya= (Button)findViewById(R.id.Kriya);
+        News_Feed=(Button)findViewById(R.id.News_Feed);
+        project=(Button)findViewById(R.id.project) ;
+        Members=(Button)findViewById(R.id.Members);
+        Members.setOnClickListener(new View.OnClickListener() {
+@Override
+            public void onClick(View v) {
+                Intent memb = new Intent(MainActivity.this,Member.class);
+                startActivity(memb);
+
+            }
+
+
+
+        });
+About_Us.setOnClickListener(    new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        Intent about = new Intent(MainActivity.this,about_us.class);
+        startActivity(about);
+    }
+});
     }
 }
---
+++ app/src/main/java/com/club/yes_plus/about_us.java
@@ -0,0 +1,43 @@
+package com.club.yes_plus;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class about_us extends AppCompatActivity {
+TextView title_1;
+TextView title_2;
+TextView text_1;
+TextView text_2;
+TextView text_3;
+    TextView text_4;TextView text_5;TextView text_6;TextView text_7;TextView text_8;TextView text_9;TextView text_10;
+    TextView title_3;TextView title_4;TextView title_5;TextView title_6;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+
+        title_1=(TextView)findViewById(R.id.title_1);
+        title_2=(TextView)findViewById(R.id.title_2);
+        text_1=(TextView)findViewById(R.id.text_1);
+        text_2=(TextView)findViewById(R.id.text_2);
+        text_3=(TextView)findViewById(R.id.text_3);
+        text_4=(TextView)findViewById(R.id.text_4);
+        text_5=(TextView)findViewById(R.id.text_5);
+        text_6=(TextView)findViewById(R.id.text_6);
+        text_7=(TextView)findViewById(R.id.text_7);
+        text_8=(TextView)findViewById(R.id.text_8);
+        text_9=(TextView)findViewById(R.id.text_9);
+        text_10=(TextView)findViewById(R.id.text_10);
+        title_3=(TextView)findViewById(R.id.title_3);
+        title_4=(TextView)findViewById(R.id.title_4);
+        title_5=(TextView)findViewById(R.id.title_5);
+        title_6=(TextView)findViewById(R.id.title_6);
+
+
+
+
+
+
+    }
+}
